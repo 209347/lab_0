bf38ec9a38ad1f7313eb3a40f85d41b53b9a820f
Kalinowski.Mikolaj <209347@edu.p.lodz.pl>
2019-03-07 18:25:05 +0100

    Implementacja metody tails

--- src/main/java/edu/iis/mto/lab_0/TailsGenerator.java
+++ src/main/java/edu/iis/mto/lab_0/TailsGenerator.java
@@ -6,7 +6,14 @@ import java.util.List;
 public class TailsGenerator {
 
     public List<String> tails(String value) {
-        return new ArrayList<>(value.length());
+        if(value == null)
+            return new ArrayList<>();
+
+        List<String> cutString = new ArrayList<String>(value.length() + 1);
+        for (int i = 0; i < value.length() + 1; i++) {
+            cutString.add(value.substring(i));
+        }
+        return cutString;
     }
 
 }
