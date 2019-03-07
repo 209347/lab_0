243041ddd124603cbf1320b5fd4c55769ba1ed2b
Kalinowski.Mikolaj <209347@edu.p.lodz.pl>
2019-03-07 18:39:05 +0100

    Poprawka implementacji metody tails

--- src/main/java/edu/iis/mto/lab_0/TailsGenerator.java
+++ src/main/java/edu/iis/mto/lab_0/TailsGenerator.java
@@ -1,13 +1,15 @@
 package edu.iis.mto.lab_0;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class TailsGenerator {
 
     public List<String> tails(String value) {
-        if(value == null)
-            return new ArrayList<>();
+        if (value == null) {
+            return Collections.emptyList();
+        }
 
         List<String> cutString = new ArrayList<String>(value.length() + 1);
         for (int i = 0; i < value.length() + 1; i++) {
